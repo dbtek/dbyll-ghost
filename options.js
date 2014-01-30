351b0f7d8d038dcf1a2a60b830b8d86bd7ed2125
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-01-30 23:37:11 +0200

    initial commit

---
+++ options.js
@@ -0,0 +1,15 @@
+/**
+ * Dbyll Theme Options
+ */
+
+var site = {
+  
+  author: {
+    email: "your@email",
+    github: "abc123",
+    twitter: "abc123",
+    pinterest: "abc123",
+    linkedin: "abc123"
+  }
+  
+}
