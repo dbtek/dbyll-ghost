274ca0e0a0fcbc1d20e34f9cf8c1dddd8dcb47db
Ismail Demirbilek <ce.demirbilek@gmail.com>
2014-02-03 12:16:36 +0200

    height settings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,23 @@
 $( document ).ready(function() {
 
-	/* Sidebar height set */
-	$('.sidebar').css('min-height',$(document).height());
+  var documentHeight = $(document).height();
+  var footerHeight = $('.site-footer').height();
 
-	/* Secondary contact links */
-	var scontacts = $('#contact-list-secondary');
-	var contact_list = $('#contact-list');
-	
-	scontacts.hide();
-	
-	contact_list.mouseenter(function(){ scontacts.slideDown(); });
-	
-	contact_list.mouseleave(function(){ scontacts.slideUp(); });
+  /* Sidebar height set */
+  $('.sidebar').css('min-height', documentHeight);
+
+  /* Content height set */
+
+  $('.content').css('min-height', documentHeight - 2*footerHeight - 80);
+
+  /* Secondary contact links */
+  var scontacts = $('#contact-list-secondary');
+  var contact_list = $('#contact-list');
+
+  scontacts.hide();
+
+  contact_list.mouseenter(function(){ scontacts.slideDown(); });
+
+  contact_list.mouseleave(function(){ scontacts.slideUp(); });
 
 });
